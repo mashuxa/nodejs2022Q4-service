da51dc4352e801c375b23ee56a89bcd17a9db498
masha.kul <m.kulikova@itransition.com>
2023-02-15 21:04:51 +0300

    feat: Remake artist service to typeorm

--- src/entities/artists/artist.module.ts
+++ src/entities/artists/artist.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ArtistController } from './artists.controller';
 import { DBModule } from '../../db/db.module';
 import { ArtistsService } from './artists.service';
+import artistProviders from '../../db/entities/artists/artist.providers';
 
 @Module({
   controllers: [ArtistController],
-  providers: [ArtistsService],
+  providers: [...artistProviders, ArtistsService],
   imports: [DBModule],
 })
 export class ArtistModule {}
--- src/entities/artists/artists.service.ts
+++ src/entities/artists/artists.service.ts
@@ -1,49 +1,64 @@
-import { DB } from '../../db/DB';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
+import { ARTIST_REPOSITORY } from '../../db/entities/artists/constants';
+import { TRACK_REPOSITORY } from '../../db/entities/tracks/constants';
+import { ALBUM_REPOSITORY } from '../../db/entities/albums/constants';
+import { Track } from '../../db/entities/tracks/track';
+import { Artist } from '../../db/entities/artists/artist';
+import { Album } from '../../db/entities/albums/album';
 
 @Injectable()
 export class ArtistsService {
-  constructor(private readonly db: DB) {}
+  constructor(
+    @Inject(ARTIST_REPOSITORY)
+    private artistRepository: Repository<Artist>,
+    @Inject(TRACK_REPOSITORY)
+    private trackRepository: Repository<Track>,
+    @Inject(ALBUM_REPOSITORY)
+    private albumRepository: Repository<Album>,
+  ) {}
 
   async findAll() {
-    return this.db.artists.findAll();
+    return this.artistRepository.find();
   }
 
   async findOne(id: string) {
-    return this.db.artists.findById(id);
+    return this.artistRepository.findOneBy({ id });
   }
 
-  async create(createArtistDto: CreateArtistDto) {
-    return this.db.artists.create(new CreateArtistDto(createArtistDto));
+  async create(artist: CreateArtistDto) {
+    return this.artistRepository.create(artist);
   }
 
   async update(id: string, updateArtistDto: UpdateArtistDto) {
-    const artist = this.db.artists.findById(id);
+    const artist = this.artistRepository.findOneBy({ id });
 
     if (!artist) return;
 
-    return this.db.artists.update(id, updateArtistDto);
+    return this.artistRepository.update(id, updateArtistDto);
   }
 
   async delete(id: string) {
-    const artist = this.db.artists.findById(id);
+    const artist = this.artistRepository.findOneBy({ id });
 
     if (!artist) return;
 
-    this.db.artists.remove(id);
+    await this.artistRepository.delete(id);
 
-    const updatedTracks = this.db.tracks
-      .findMany('artistId', id)
-      .map((track) => ({ ...track, artistId: null }));
-    const updatedAlbums = this.db.albums
-      .findMany('artistId', id)
-      .map((artist) => ({ ...artist, artistId: null }));
+    this.trackRepository
+      .createQueryBuilder()
+      .update(Track)
+      .set({ artistId: null })
+      .where({ artistId: id });
+    this.albumRepository
+      .createQueryBuilder()
+      .update(Album)
+      .set({ artistId: null })
+      .where({ artistId: id });
 
-    this.db.tracks.updateMany(updatedTracks);
-    this.db.albums.updateMany(updatedAlbums);
-    this.db.favorites.artists.remove(id);
+    // this.db.favorites.artists.remove(id);
 
     return '';
   }
